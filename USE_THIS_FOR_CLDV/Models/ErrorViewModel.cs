080be2705c3b8d785923d87305de829451f63e09
ganee <ganee@DESKTOP-DUGRL9H>
2024-04-02 19:13:42 +0200

    Add project files.

---
+++ USE_THIS_FOR_CLDV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace USE_THIS_FOR_CLDV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
